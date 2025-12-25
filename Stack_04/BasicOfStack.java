6b5a0475f98f51b2905ca3bec441b0133d47f5eb
Vaibhav <vaibhav.cse006@gmail.com>
2025-12-25 16:25:24 +0530

    add basics of stack

---
+++ Stack_04/BasicOfStack.java
@@ -0,0 +1,21 @@
+package Stack_04;
+import java.util.*;
+public class BasicOfStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        System.out.println(st);
+        st.push(30);
+        st.push(40);
+        System.out.println(st);
+        System.out.println(st+"-> "+st.peek()+ " "+ st.size());
+        st.pop();
+        st.pop();
+        System.out.println(st+"-> "+st.peek()+ " "+ st.size());
+        st.pop();
+        System.out.println(st+"-> "+st.peek()+ " "+ st.size());
+        st.pop();
+        System.out.println(st+"-> "+ st.size());
+    }
+}

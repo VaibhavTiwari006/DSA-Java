48d2bbeabf0910c92ee0715e5bbbe5b132ffee1e
Vaibhav <vaibhav.cse006@gmail.com>
2026-03-15 10:53:16 +0530

    Add Quick Sort Program

---
+++ Sorting_05/QuickSort.java
@@ -0,0 +1,50 @@
+package Sorting_05;
+public class QuickSort {
+    public static void main(String args[]) {
+        int[] arr = { 15, 9, 7, 13, 12, 16, 4, 18, 11 };
+        int len = arr.length;
+        QuickSort obj = new QuickSort();
+        obj.quickSortRecursion(arr, 0, len - 1);
+        obj.printArray(arr);
+
+    }
+
+    int partition(int[] arr, int low, int high) {
+        int pivot = arr[(low + high) / 2]; // middle elemnt
+
+        while (low <= high) {
+            while (arr[low] < pivot) {
+                low++;
+            }
+            while (arr[high] > pivot) {
+                high--;
+            }
+            if (low <= high) {
+                int temp = arr[low];
+                arr[low] = arr[high];
+                arr[high] = temp;
+
+                low++;
+                high--;
+            }
+        }
+        return low;
+
+    }
+
+    void quickSortRecursion(int[] arr, int low, int high) {
+        int pi = partition(arr, low, high);
+        if (low < pi - 1) {
+            quickSortRecursion(arr, low, pi - 1);
+        }
+        if (pi < high) {
+            quickSortRecursion(arr, pi, high);
+        }
+    }
+
+    void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}

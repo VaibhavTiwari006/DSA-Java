b198ae441e3bf13a83be8ad443cb687cd87caa0f
Vaibhav <vaibhav.cse006@gmail.com>
2026-07-22 16:29:24 +0530

    Add recursive solution for LeetCode 509: Fibonacci Number

---
+++ Leetcode/P0509FibonacciNumber.java
@@ -0,0 +1,11 @@
+package Leetcode;
+public class P0509FibonacciNumber {
+    public int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+}

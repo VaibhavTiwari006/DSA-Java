bb72e01f091f50c2de0bb59ce04d2389e9ffb30d
Vaibhav <vaibhav.cse006@gmail.com>
2026-07-23 10:22:42 +0530

    solve LeetCode 9: Palindrome Number

---
+++ Leetcode/P0009_PalindromeNumber.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+class P0009_PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x > 0) {
+            int rem = x % 10;
+            reverse = reverse * 10 + rem;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+}
--- Leetcode/P0509_FibonacciNumber.java
+++ Leetcode/P0509_FibonacciNumber.java
@@ -1,5 +1,5 @@
 package Leetcode;
-public class P0509FibonacciNumber {
+public class P0509_FibonacciNumber {
     public int fib(int n) {
         if (n == 0)
             return 0;

3548eade233e1a67302553fa41ea5599b74b5564
Vaibhav <vaibhav.cse006@gmail.com>
2026-07-24 19:02:14 +0530

    add solution for LeetCode 504 - Base 7

---
+++ Leetcode/P0007_Base7.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+public class P0007_Base7 {
+    public String convertToBase7(int num) {
+        if(num == 0) return "0";
+
+        boolean negative = num < 0;
+        num = Math.abs(num);
+
+        StringBuilder ans = new StringBuilder();
+        while (num > 0){
+        ans.append(num % 7);
+        num /= 7;
+        }
+
+        if(negative)  ans.append('-');
+        return ans.reverse().toString();
+    }       
+  }

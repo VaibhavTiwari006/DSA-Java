2485609fc3eb93bb67bae3df876853d1f07ca26a
Vaibhav <vaibhav.cse006@gmail.com>
2026-08-01 15:22:18 +0530

    Solve LeetCode 2469: Convert the Temperature in Java

---
+++ Leetcode/P2469_ConvertTheTemperature.java
@@ -0,0 +1,9 @@
+package Leetcode;
+public class P2469_ConvertTheTemperature {
+    public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+
+        return new double[]{kelvin, fahrenheit};
+    } 
+}

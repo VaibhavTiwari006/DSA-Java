8c6e5f88788c194c0f5d62b5f755203894aab998
Vaibhav <vaibhav.cse006@gmail.com>
2026-08-02 23:17:20 +0530

    Solve LeetCode 1672: Richest Customer Wealth

---
+++ Leetcode/P1431_KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,16 @@
+package Leetcode;
+public class P1431_KidsWithTheGreatestNumberOfCandies {
+    public int maximumWealth(int[][] accounts) {
+        int ans=0;
+        for(int i=0; i<accounts.length; i++){
+            int wealth =0;
+            for(int j=0; j<accounts[i].length; j++){
+              wealth += accounts[i][j];
+            }
+            if(wealth > ans){
+                ans = wealth;
+            }
+        }
+        return ans;
+    }
+}

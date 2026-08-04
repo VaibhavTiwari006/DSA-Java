011fb87aedd98167b462c788323f00021fc992e5
Vaibhav <vaibhav.cse006@gmail.com>
2026-08-04 16:25:32 +0530

    Solve LeetCode 20: Valid Parentheses using stack

---
+++ Leetcode/P0020ValidParentheses.java
@@ -0,0 +1,29 @@
+package Leetcode;
+import java.util.Stack;
+public class P0020ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            }
+            else if (ch == ')') {
+                if (st.isEmpty() || st.peek() != '(')
+                    return false;
+                st.pop();
+            }
+            else if (ch == ']') {
+                if (st.isEmpty() || st.peek() != '[')
+                    return false;
+                st.pop();
+            }
+            else if (ch == '}') {
+                if (st.isEmpty() || st.peek() != '{')
+                    return false;
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}

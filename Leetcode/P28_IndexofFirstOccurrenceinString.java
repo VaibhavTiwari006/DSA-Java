c8ae9a1dbc7ec00c501ede61506dd7874bd712f3
Vaibhav <vaibhav.cse006@gmail.com>
2026-08-18 11:33:29 +0530

    Solve Leetcode 28 : Indexo of First Occurrence in String

---
+++ Leetcode/P28_IndexofFirstOccurrenceinString.java
@@ -0,0 +1,8 @@
+package Leetcode;
+public class P28_IndexofFirstOccurrenceinString {
+    class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);        
+    }
+}   
+}

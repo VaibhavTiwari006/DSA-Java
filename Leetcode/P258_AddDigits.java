0df054afaec9f070072dc3eda0404a10a6f49aa3
Vaibhav <vaibhav.cse006@gmail.com>
2026-08-19 23:09:41 +0530

    Solve Leetcode 258 : Add Digits using repeated digit sum

---
+++ Leetcode/P258_AddDigits.java
@@ -0,0 +1,17 @@
+package Leetcode;
+public class P258_AddDigits {
+    class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                int dig = num % 10;
+                sum += dig;
+                num = num / 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
+}

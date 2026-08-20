9e768d92629d61973a64e27d6544d185eb6fb9d2
Vaibhav <vaibhav.cse006@gmail.com>
2026-08-21 01:55:38 +0530

    Solve Leetcode 326 : Power Of Three

---
+++ Leetcode/P326_PowerOfThree.java
@@ -0,0 +1,14 @@
+package Leetcode;
+public class P326_PowerOfThree {
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+        return n == 1;
+    }
+}
+}

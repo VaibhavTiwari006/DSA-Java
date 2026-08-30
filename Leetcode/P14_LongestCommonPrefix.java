7984be59e4df476eebb6e3d758e7614de47ea27a
Vaibhav <vaibhav.cse006@gmail.com>
2026-08-30 17:04:08 +0530

    Solve LeetCode 14 Longest Common Prefix

---
+++ Leetcode/P14_LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+public class P14_LongestCommonPrefix {
+    class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int i =0 ; i < strs.length; i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix = prefix.substring(0,prefix.length()-1);
+                if(prefix.length()==0){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
+    
+}

cec54d01930981d26766f841534f84bf69547085
Vaibhav <vaibhav.cse006@gmail.com>
2026-09-02 18:46:41 +0530

    Solve Construct Uniform Parity Array I

---
+++ Leetcode/P3875_ConstructUniformParityArrayI.java
@@ -0,0 +1,10 @@
+package Leetcode;
+
+public class P3875_ConstructUniformParityArrayI {
+    class Solution {
+        public boolean uniformArray(int[] nums1) {
+            return true;
+        }
+    }
+
+}

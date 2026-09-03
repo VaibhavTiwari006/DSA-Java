9bfd895e8690068aa01ba594c0adc4a1afc25ce6
Vaibhav <vaibhav.cse006@gmail.com>
2026-09-03 16:32:24 +0530

    Solve LeetCode 3876 - Construct Uniform Parity Array II

---
+++ Leetcode/P3876_ConstructUniformParityArrayII.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+public class P3876_ConstructUniformParityArrayII {
+    class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min = nums1[0];
+        for (int i = 1; i < nums1.length; i++) {
+            if (nums1[i] < min) {
+                min = nums1[i];
+            }
+        }
+        if (min % 2 == 1) {
+            return true;
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] % 2 == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+}

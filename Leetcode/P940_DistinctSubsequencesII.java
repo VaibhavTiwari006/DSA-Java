6f548220db1b048b380c0d75a9c53a24ab9c6d7f
Vaibhav <vaibhav.cse006@gmail.com>
2026-09-07 23:39:34 +0530

    Solve Leetcode 940 : Distinct SubSequences 2

---
+++ Leetcode/P940_DistinctSubsequencesII.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+import java.util.HashMap;
+
+public class P940_DistinctSubsequencesII {
+    class Solution {
+    public int distinctSubseqII(String s) {
+        long MOD = 1_000_000_007;
+        long dp[] = new long[s.length()+1];
+        dp[0]=1;
+        HashMap<Character , Integer> lo = new HashMap<>();
+        for(int i=1; i< dp.length; i++){
+            char ch = s.charAt(i-1);
+            dp[i] = (2 * dp[i - 1]) % MOD;
+            if(lo.containsKey(ch)){
+                int j = lo.get(ch);
+                dp[i] = (dp[i] - dp[j - 1] + MOD) % MOD;
+            }
+            lo.put(ch,i);
+        }
+        return (int) ((dp[s.length()] - 1 + MOD) % MOD);
+    }
+}
+}

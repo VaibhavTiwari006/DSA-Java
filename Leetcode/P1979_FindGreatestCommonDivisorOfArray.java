719e3f9c27a9a9aead2b511f682ae60a433e03d0
Vaibhav <vaibhav.cse006@gmail.com>
2026-09-10 19:39:25 +0530

    Solve Leetcode 1979 : Find Greatest Comman Divisor Of Array

---
+++ Leetcode/P1979_FindGreatestCommonDivisorOfArray.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+public class P1979_FindGreatestCommonDivisorOfArray {
+    class Solution {
+    public int findGCD(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+        while(min!=0){
+            int rem = max % min;
+            max = min;
+            min = rem;
+        }
+        return max;
+    }
+}
+}
+    

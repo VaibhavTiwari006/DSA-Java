f795a08aabd6b0bc4cb9d855822d2a68d1eb5076
Vaibhav <vaibhav.cse006@gmail.com>
2026-09-13 22:38:50 +0530

    Solve Leetcode 835 : Image overlap

---
+++ Leetcode/P835_ImageOverlap.java
@@ -0,0 +1,32 @@
+package Leetcode;
+
+public class P835_ImageOverlap {
+    class Solution {
+        public int largestOverlap(int[][] img1, int[][] img2) {
+            int n = img1.length;
+            int[][] shifts = new int[2 * n - 1][2 * n - 1];
+            int maxOverlap = 0;
+            for (int r1 = 0; r1 < n; r1++) {
+                for (int c1 = 0; c1 < n; c1++) {
+                    if (img1[r1][c1] == 0)
+                        continue;
+
+                    for (int r2 = 0; r2 < n; r2++) {
+                        for (int c2 = 0; c2 < n; c2++) {
+                            if (img2[r2][c2] == 0)
+                                continue;
+
+                            int rowShift = r2 - r1 + n - 1;
+                            int colShift = c2 - c1 + n - 1;
+
+                            shifts[rowShift][colShift]++;
+                            maxOverlap = Math.max(
+                                    maxOverlap, shifts[rowShift][colShift]);
+                        }
+                    }
+                }
+            }
+            return maxOverlap;
+        }
+    }
+}

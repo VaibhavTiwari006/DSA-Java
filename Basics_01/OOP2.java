71108e6e62f9b0173e87252d0a82ffaab8fa5503
Vaibhav <vaibhav.cse006@gmail.com>
2026-09-23 12:40:34 +0530

    Implement the concept of Construccctors

---
+++ Basics_01/OOP2.java
@@ -0,0 +1,33 @@
+package Basics_01;
+
+public class OOP2 {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.name = "Vaibhav";
+        s1.age = 19;
+        s1.roll = 303;
+        s1.college = "CU";
+
+        //Constructors --> To create an object
+
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+        System.out.println(s1.roll);
+        System.out.println(s1.college);
+
+        int x; //local variable (stored in stack memeory) --> no default variables
+    }
+}
+
+class Student {
+    String name; // information/data/characteristics --> instance variable
+    int age;     // stored in heap --. Have default variables
+    int roll;
+    String college;
+
+    void markAttendence() {  //behaviours --> functions --> instance method
+        System.out.println("Attendece marked by " + name);
+    }
+    
+}
